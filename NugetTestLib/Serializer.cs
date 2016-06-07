19062b1e86bb3e8c322261333ec494ab223582fe
Melik <mkavcar@gmail.com>
2016-06-07 11:04:22 -0400

    NugetTestLib

---
+++ NugetTestLib/Serializer.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace NugetTestLib
+{
+    public class Serializer
+    {
+        public static string toJson(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public static T fromJson<T>(string obj)
+        {
+            return JsonConvert.DeserializeObject<T>(obj);
+        }
+    }
+}
