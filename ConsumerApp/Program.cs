f0b246dbca020b2bc53a502b8658adf11ddc3ff0
Melik <mkavcar@gmail.com>
2016-06-07 11:02:00 -0400

    ConsumerApp

---
+++ ConsumerApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NugetTestLib;
+
+namespace ConsumerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+                Console.WriteLine(Serializer.toJson(new { Field1 = args[0] }));
+
+            Console.ReadLine();
+        }
+    }
+}
